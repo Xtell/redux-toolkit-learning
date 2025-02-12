ab9432e4acb567469056a5502679b3a69b9ae9e3
Andrey Evstafyev <evstafyevan96@gmail.com>
2025-02-12 09:56:03 +0500

    firebase p1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from "firebase/analytics";
 import './index.css'
 import App from './App.tsx'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhluNu_F2GINpHSPDItm4wy0_pAZHwqMg",
+  authDomain: "simple-todo-af97c.firebaseapp.com",
+  projectId: "simple-todo-af97c",
+  storageBucket: "simple-todo-af97c.firebasestorage.app",
+  messagingSenderId: "1008583984212",
+  appId: "1:1008583984212:web:f059fc42ae0a94043b59c1",
+  measurementId: "G-3GBXEYQ6XV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
